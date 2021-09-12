3a08a4127106968bc3ce1f7bcd808d1a83d82ce2
Merlini <merlini.jayson@gmail.com>
2021-09-12 15:06:46 +0200

    test

---
+++ src/data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "CSCB",
+    date: "Janvier 2021",
+    languages: ["Wordpress", "CSS"],
+    infos:
+      "Site Web / Blog de l'association du Club Sportif Courtry Badminton.",
+    img: "./portefolio/assets/img/cscb2.PNG",
+    link: "http://www.cscb.towebapp.fr",
+  },
+  {
+    id: 2,
+    title: "Hypertube",
+    date: "Juin 2021",
+    languages: ["AngularJs", "Material", "HTML/CSS"],
+    infos:
+      "[Projet Ecole 42 | En groupe] Site de streaming avec des torrents.",
+    img: "./portefolio/assets/img/hypertube.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Matcha",
+    date: "Novembre 2020",
+    languages: ["Javascript", "jQuery", "Bootstrap", "HTML/CSS"],
+    infos:
+      "[Projet Ecole 42 | En groupe] Site de rencontre.",
+    img: "./portefolio/assets/img/matcha.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 4,
+    title: "Gomoku",
+    date: "En cours",
+    languages: ["ReactJs", "HTML/CSS", "Redux"],
+    infos:
+      "[Projet Ecole 42 | En groupe] Jeu du gomoku avec IA.",
+    img: "./portefolio/assets/img/maintenance.jpg",
+    link: "http://www.google.com",
+  },
+];
