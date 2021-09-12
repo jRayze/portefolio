676521a778dbe8238f249ef5ccca6b28f6cd3d0a
Merlini <merlini.jayson@gmail.com>
2021-09-12 14:51:07 +0200

    Updates

---
+++ precache-manifest.3b7e75c9eee9b322cde370ad703114c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f54ec9b93d16d1dec1e385b0b3aced1",
+    "url": "/portefolio/index.html"
+  },
+  {
+    "revision": "80646d4bee1069da9e56",
+    "url": "/portefolio/static/css/main.66941193.chunk.css"
+  },
+  {
+    "revision": "9003e99fccf887cce604",
+    "url": "/portefolio/static/js/2.94726665.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/portefolio/static/js/2.94726665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80646d4bee1069da9e56",
+    "url": "/portefolio/static/js/main.3024f704.chunk.js"
+  },
+  {
+    "revision": "26409fac1b9a0054a096",
+    "url": "/portefolio/static/js/runtime-main.16e2b163.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/portefolio/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/portefolio/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
